1ee03efe4d57dec1926d0e095bdb406618f1a858
Шейко Даниил Дмитриевич <208482@edu.fa.ru>
2024-04-15 18:11:33 +0300

    first commit

---
+++ WpfApp1/Window1.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // Проверка на заполнение всех полей
+            if (string.IsNullOrEmpty(txtLastName.Text) ||
+                string.IsNullOrEmpty(txtMail.Text) ||
+                string.IsNullOrEmpty(txtLogin.Text) ||
+                string.IsNullOrEmpty(txtPassword.Password))
+            {
+                MessageBox.Show("Пожалуйста, заполните все поля.", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            string lastName = txtLastName.Text;
+            string mail = txtMail.Text;
+            string login = txtLogin.Text;
+            string password = txtPassword.Password;
+
+
+
+
+            MessageBox.Show("Регистрация успешна!");
+
+
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+
+
+            Close();
+        }
+    }
+}

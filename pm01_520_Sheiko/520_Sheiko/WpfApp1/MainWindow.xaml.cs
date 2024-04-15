c388b3b243a1f6ebcd1066c33682b5f0c1649be0
Crymini1 <167108521+Crymini1@users.noreply.github.com>
2024-04-15 18:14:25 +0300

    Add files via upload

---
+++ pm01_520_Sheiko/520_Sheiko/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Window1 authorizationWindow = new Window1();
+            authorizationWindow.ShowDialog();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtLogin.Text) ||
+                string.IsNullOrEmpty(txtPassword.Password))
+            {
+                MessageBox.Show("Пожалуйста, заполните все поля.", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+
+            string login = txtLogin.Text;
+            string password = txtPassword.Password;
+
+
+
+
+            MessageBox.Show("Вы успешно вошли!");
+            Window2 mainWindow = new Window2();
+            mainWindow.Show();
+
+        }
+    }
+}
